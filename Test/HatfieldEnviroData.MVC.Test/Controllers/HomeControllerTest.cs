61d23305f4f9b53acf3b108e998aa6fac7f0a089
Daniel <luzhanxun.daniel@gmail.com>
2015-02-24 10:31:25 -0800

    Add Unit test project for MVC project
    Add HatfieldEnviroData.MVC.Test project

---
+++ Test/HatfieldEnviroData.MVC.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.MVC.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace HatfieldEnviroData.MVC.Test.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+    }
+}
