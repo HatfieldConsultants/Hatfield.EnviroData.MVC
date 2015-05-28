381a80cb9bf7efa2ae57d1f36121d72d1285a327
Daniel <luzhanxun.daniel@gmail.com>
2015-05-28 15:53:42 -0700

    Implement upload files to import
    Interface allow to upload files to import.
    Save data is not implemented yet.
    #8

--- Source/Hatfield.EnviroData.MVC/Controllers/API/ESDATImportAPIController.cs
+++ Source/Hatfield.EnviroData.MVC/Controllers/API/ESDATImportAPIController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web;
 
 using Hatfield.EnviroData.Core;
 using Hatfield.EnviroData.MVC.Models;
@@ -26,6 +27,32 @@ namespace Hatfield.EnviroData.MVC.Controllers
         public ESDATImportAPIController(IDbContext dbContext)
         {
             _dbContext = dbContext;    
+        }
+
+        [HttpPost]
+        public IEnumerable<ResultMessageViewModel> ImportLocalFiles()
+        {
+            var request = HttpContext.Current.Request;
+
+            var headerFile = request.Files[0];
+            var sampleFile = request.Files[1];
+            var chemistryFile = request.Files[2];
+
+            var headerFileData = new DataFromFileSystem(headerFile.FileName, headerFile.InputStream);
+            var headerFileXMLFileToImport = new XMLDataToImport(headerFileData);
+
+            var sampleFileData = new DataFromFileSystem(sampleFile.FileName, sampleFile.InputStream);
+            var sampleFileCSVFileToImport = new CSVDataToImport(sampleFileData);
+
+            var chemistryFileData = new DataFromFileSystem(chemistryFile.FileName, chemistryFile.InputStream);
+            var chemistryCSVFileToImport = new CSVDataToImport(chemistryFileData);
+
+            var esdatDataToImport = new ESDATDataToImport(headerFileXMLFileToImport, sampleFileCSVFileToImport, chemistryCSVFileToImport);
+            var importer = ESDATDataImportHelper.BuildESDATDataImporter();
+
+            var results = PersistESDATData(esdatDataToImport, importer);
+
+            return results;
         }
 
         [HttpPost]
---
+++ Source/Hatfield.EnviroData.MVC/Scripts/Knockout/LocalFileImport.js
@@ -0,0 +1,33 @@
+﻿var LocalFileImportModel = function () {
+    var self = this;
+    
+    self.ImportResults = ko.observableArray();
+
+    self.submitImportRequest = function () {
+        self.ImportResults(null);
+
+        var formData = new FormData();
+        // Main magic with files here
+        var files = $('input[type=file]');
+        formData.append('headerFileInput', $('input[type=file]')[0].files[0]);
+        formData.append('sampleFileInput', $('input[type=file]')[1].files[0]);
+        formData.append('chemistryFileInput', $('input[type=file]')[2].files[0]);
+
+        $.ajax({
+            url: '../api/ESDATImportAPI/ImportLocalFiles',
+            type: 'POST',
+            data: formData,
+            mimeType: "multipart/form-data",
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {                
+                self.ImportResults(data);
+                
+            },
+            error: function (data) {
+                alert('Import local file system fail');
+            }
+        });//end of ajax
+    };
+};
