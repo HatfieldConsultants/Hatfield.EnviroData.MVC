43a70940b21339acb1a16e580cdefe88dd4fd9bc
Daniel <luzhanxun.daniel@gmail.com>
2015-06-01 11:59:21 -0700

    display imported data in ESDAT format
    display imported data in ESDAT format view and js parts
    #11

--- Source/Hatfield.EnviroData.MVC/Scripts/Knockout/SampleCollectionActionDisplay.js
+++ Source/Hatfield.EnviroData.MVC/Scripts/Knockout/SampleCollectionActionDisplay.js
@@ -2,7 +2,8 @@
     var self = this;
 
     self.CollectionActions = ko.observableArray();
-    self.ESDATDataViewModel = ko.observable();
+    self.SampleFileData = ko.observableArray();
+    self.ChemistryData = ko.observableArray();
     self.ESDATHeaderXMLContent = ko.observable('');
 
     self.FetchCollectionAction = function () {
@@ -22,7 +23,7 @@
         });//end of ajax
     };//end of FetchCollectionAction
 
-    self.FetchCollectionActionInESDAT = function (id) {        
+    self.FetchCollectionActionInESDAT = function (id) {
         $.ajax({
             url: '../api/QueryDataAPI/GetSampleCollectionActionInESDAT/' + id,
             type: 'GET',
@@ -30,53 +31,52 @@
             async: false,
             cache: false,
             contentType: 'application/json',
-            success: function (data) {
-                self.ESDATDataViewModel = ko.mapping.fromJS(data);
-
+            success: function (data) {                                
+                ko.mapping.fromJS(data.SampleFileData, {}, self.SampleFileData);
+                ko.mapping.fromJS(data.ChemistryData, {}, self.ChemistryData);
                 self.ESDATHeaderXMLContent(GenerateHeaderFileXMLContent(data));
+
             },
             error: function (data) {
                 alert("Fetch sample collection data in ESDAT format fail, please try again");
             }
         });//end of ajax
-
-        function GenerateHeaderFileXMLContent(esdatModel) {
-            var xmlContent = '<?xml version="1.0" encoding="utf-8"?>';
-            xmlContent += '<ESdat generated="" ';
-            xmlContent += 'fileType="eLabResultsHeader" ';
-            xmlContent += 'schemaVersion="1.0.1" ';
-            xmlContent += 'xmlns="http://www.escis.com.au/2013/XML"> ';
-
-            xmlContent += '<LabReport Lab_Report_Number="' + esdatModel.LabReportNumber + '" ';
-            xmlContent += 'Date_Reported="' + esdatModel.DateReported + '" ';
-            xmlContent += 'Project_ID="' + esdatModel.ProjectId + '" ';
-            xmlContent += 'Project_Number="' + esdatModel.ProjectNumber + '" ';
-            xmlContent += 'Lab_Name="' + esdatModel.LabName + '" ';
-            xmlContent += 'Lab_Signatory="' + esdatModel.LabSignatory + '">';
-
-            xmlContent += '<Associated_Files xmlns="http://www.escis.com.au/2013/XML/LabReport"/>';
-            xmlContent += '<Copies_Sent_To xmlns="http://www.escis.com.au/2013/XML/LabReport"/>';
-
-            xmlContent += '<eCoCs xmlns="http://www.escis.com.au/2013/XML/LabReport">';
-            xmlContent += '<eCoC SDG_ID="' + esdatModel.SDGID + '" ';
-            xmlContent += 'CoC_Number="' + esdatModel.COCNumber + '">';
-            xmlContent += '<Lab_Requests>';
-
-            xmlContent += '<Lab_Request ID="' + esdatModel.LabRequestId + '" ';
-            xmlContent += 'Number="' + esdatModel.LabRequestNumber + '" ';
-            xmlContent += 'Version="' + esdatModel.LabRequestVersion + '"/>';
-
-
-            xmlContent += '</Lab_Requests>';
-            xmlContent += '</eCoC>';
-            xmlContent += '</eCoCs>';
-            xmlContent += '</LabReport>';
-            xmlContent += '</ESdat>';
-
-            return xmlContent;
-        }//end of GenerateHeaderFileXMLContent
-
     };//end of FetchCollectionActionInESDAT
+
+    function GenerateHeaderFileXMLContent(esdatModel) {
+        var xmlContent = '<?xml version="1.0" encoding="utf-8"?>';
+        xmlContent += '<ESdat generated="" ';
+        xmlContent += 'fileType="eLabResultsHeader" ';
+        xmlContent += 'schemaVersion="1.0.1" ';
+        xmlContent += 'xmlns="http://www.escis.com.au/2013/XML"> ';
+
+        xmlContent += '<LabReport Lab_Report_Number="' + esdatModel.LabReportNumber + '" ';
+        xmlContent += 'Date_Reported="' + esdatModel.DateReported + '" ';
+        xmlContent += 'Project_ID="' + esdatModel.ProjectId + '" ';
+        xmlContent += 'Project_Number="' + esdatModel.ProjectNumber + '" ';
+        xmlContent += 'Lab_Name="' + esdatModel.LabName + '" ';
+        xmlContent += 'Lab_Signatory="' + esdatModel.LabSignatory + '">';
+
+        xmlContent += '<Associated_Files xmlns="http://www.escis.com.au/2013/XML/LabReport"/>';
+        xmlContent += '<Copies_Sent_To xmlns="http://www.escis.com.au/2013/XML/LabReport"/>';
+
+        xmlContent += '<eCoCs xmlns="http://www.escis.com.au/2013/XML/LabReport">';
+        xmlContent += '<eCoC SDG_ID="' + esdatModel.SDGID + '" ';
+        xmlContent += 'CoC_Number="' + esdatModel.COCNumber + '">';
+        xmlContent += '<Lab_Requests>';
+
+        xmlContent += '<Lab_Request ID="' + esdatModel.LabRequestId + '" ';
+        xmlContent += 'Number="' + esdatModel.LabRequestNumber + '" ';
+        xmlContent += 'Version="' + esdatModel.LabRequestVersion + '"/>';
+
+        xmlContent += '</Lab_Requests>';
+        xmlContent += '</eCoC>';
+        xmlContent += '</eCoCs>';
+        xmlContent += '</LabReport>';
+        xmlContent += '</ESdat>';
+
+        return xmlContent;
+    }//end of GenerateHeaderFileXMLContent
 };
 
 $(document).ready(function () {
