c8be288c3391ac3415beef05e11feb7b4e6dc542
gvassas <guillaume.vassas@gmail.com>
2015-07-10 14:43:13 -0700

    added CV Update link to home page.

--- Source/Hatfield.EnviroData.MVC/Controllers/HomeController.cs
+++ Source/Hatfield.EnviroData.MVC/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 
 using Hatfield.EnviroData.Core;
+using System.Configuration;
+using Hatfield.EnviroData.CVUpdater;
+using System.Xml.Linq;
 
 namespace Hatfield.EnviroData.MVC.Controllers
 {
@@ -18,6 +21,29 @@ namespace Hatfield.EnviroData.MVC.Controllers
         public ActionResult Index()
         {            
             return View();
+        }
+
+        public ActionResult CVUpdates()
+        {
+            string ApiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+            string VocabSiteUrl = ConfigurationManager.AppSettings["VocabTermsUrl"];
+
+            CVTermAPILayer parser = new CVTermAPILayer();
+            CVTermBusinessLayer biz = new CVTermBusinessLayer(new ODM2Entities());
+
+            var endpoints = parser.GetAPIEndpoints(VocabSiteUrl);
+
+            //Get data for each CV Type, extract and write to the DB
+            foreach (var endpoint in endpoints)
+            {
+                var doc = new XDocument();
+                var rawCV = parser.GetSingleCV(ApiUrl, endpoint.Value, "skos");
+                var results = parser.ImportXMLData(XDocument.Parse(rawCV));
+                biz.AddOrUpdateCVs(endpoint.Value, results.ExtractedEntities);
+                biz.CheckForDeleted(endpoint.Value, results.ExtractedEntities);
+
+            }
+            return new EmptyResult();
         }
     }
 }
