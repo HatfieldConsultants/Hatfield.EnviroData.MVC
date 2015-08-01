308ff5f6bfce9c5fc7164904c07ca62c1fad9bbf
Daniel <luzhanxun.daniel@gmail.com>
2015-07-31 17:21:52 -0700

    Add sb-admin2 template
    Add sb-admin2 template

---
+++ Source/Hatfield.EnviroData.MVC/Controllers/ControlledVocabulariesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hatfield.EnviroData.MVC.Controllers
+{
+    public class ControlledVocabulariesController : Controller
+    {
+        //
+        // GET: /ControlledVocabularies/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
