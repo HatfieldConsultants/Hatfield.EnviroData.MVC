b64c1ed3bade8dcc1e411b32cc534a8695a82645
Nima Boscarino <nboscarino@hatfieldgroup.com>
2016-09-16 17:41:01 -0700

    added selectedAnalyte/Guideline bindings, now passes form ID to backend + selections
    backend will protect most recently modified form and will update the
    others while protecting the selections

--- Retheming_WIP_Nima/Hatfield.EnviroData.MVCPrototype/Controllers/API/WQAPIController.cs
+++ Retheming_WIP_Nima/Hatfield.EnviroData.MVCPrototype/Controllers/API/WQAPIController.cs
@@ -21,51 +21,127 @@ namespace Hatfield.EnviroData.MVCPrototype.Controllers.API
         public string SiteType { get; set; }
     }
 
-    
+    public class Analyte
+    {
+        public int Id { get; set; }
+        public int LabId { get; set; }
+        public string AnalyteName { get; set; }
+        public int NumberOfDecimalPlaces { get; set; }
+        public int CategoryId { get; set; }
+        public double DetectionLimit { get; set; }
+        public string Unit { get; set; }
+    }
+
+    public class Guideline
+    {
+        public int Id { get; set; }
+        public string GuidelineName { get; set; }
+        public string GuidelineLongName { get; set; }
+    }
+
+    public class Data
+    {
+        public int Id { get; set; }
+        public string LabSampleID { get; set; }
+        public string ClientSampleID { get; set; }
+        public int WaterQualityLabAnalyteId { get; set; }
+        public DateTime SampleDateTime { get; set; }
+        public string Units { get; set; }
+        public double Result { get; set; }
+        public string ResultQualifier { get; set; }
+        public double DetectionLimit { get; set; }
+        public int LabId { get; set; }
+        public string SampleType { get; set; }
+        public string Method { get; set; }
+        public int VMVCode { get; set; }
+        public int StationId { get; set; }
+    }
+
+    public class Standard
+    {
+        public int Id { get; set; }
+        public string AnalyteNameInGuideline { get; set; }
+        public int GuidelineId { get; set; }
+        public int WaterQualityLabAnalyteId { get; set; }
+        public string Unit { get; set; }
+        public string StandardValue { get; set; }
+        public string ValueType { get; set; }
+        public double ThresholdValue { get; set; }
+        public string ReferenceSource { get; set; }
+    }
+
+    public class QueryForm
+    {
+        public IEnumerable<Analyte> visibleAnalytes { get; set; }
+        public IEnumerable<Guideline> visibleGuidelines { get; set; }
+        public IEnumerable<Site> visibleSites { get; set; }
+        public string modifiedFormId { get; set; }
+    }
+
     public class WQAPIController : ApiController
     {
 
         [Route("WQ/QueryData")]
-        [HttpGet]
-        public HttpResponseMessage Get()
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] QueryForm queryParams)
         {
-            //Note: Instead of using Object, I should create classes to represent sites, analytes, guidelines, etc....
             var response = new HttpResponseMessage();
+            
+            //Load all data which will then be queried
             string sitePath = HttpContext.Current.Server.MapPath("~/assets/site.json");
             string siteText = System.IO.File.ReadAllText(sitePath);
             var sites = JsonConvert.DeserializeObject<IEnumerable<Site>>(siteText);
             string analytePath = HttpContext.Current.Server.MapPath("~/assets/analyte.json");
             string analyteText = System.IO.File.ReadAllText(analytePath);
-            var analytes = JsonConvert.DeserializeObject<IEnumerable<Object>>(analyteText);
+            var analytes = JsonConvert.DeserializeObject<IEnumerable<Analyte>>(analyteText);
             string guidelinePath = HttpContext.Current.Server.MapPath("~/assets/guideline.json");
             string guidelineText = System.IO.File.ReadAllText(guidelinePath);
-            var guidelines = JsonConvert.DeserializeObject<IEnumerable<Object>>(guidelineText);
+            var guidelines = JsonConvert.DeserializeObject<IEnumerable<Guideline>>(guidelineText);
+            string dataPath = HttpContext.Current.Server.MapPath("~/assets/sampleData.json");
+            string dataText = System.IO.File.ReadAllText(dataPath);
+            var data = JsonConvert.DeserializeObject<IEnumerable<Data>>(dataText);
+            string standardPath = HttpContext.Current.Server.MapPath("~/assets/standard.json");
+            string standardText = System.IO.File.ReadAllText(standardPath);
+            var standards = JsonConvert.DeserializeObject<IEnumerable<Standard>>(standardText);
 
-            var querySites = from site in sites
-                                       where site.SiteType == "Surface Water"
-                                       select site;
 
-            string jsonResponse = Newtonsoft.Json.JsonConvert.SerializeObject(new { sites = querySites, analytes = analytes, guidelines = guidelines});
+            //test queries
+            //var querySites = from site in sites
+              //               select site;
+
+            var querySites = sites;
+
+            var queryAnalytes = (from analyte in analytes
+                                select analyte);
+
+            var queryGuidelines = from guideline in guidelines
+                                   select guideline;
+
+            
+            string jsonResponse = Newtonsoft.Json.JsonConvert.SerializeObject(new { sites = querySites, analytes = queryAnalytes, guidelines = queryGuidelines, message = queryParams.modifiedFormId });
             response.Content = new StringContent(jsonResponse);
+
             return response;
         }
-
-        [Route("WQ/FilterQueryForm")] //this route needs to be renamed, along with the other routes
+        [Route("WQ/LoadForm")]
         [HttpGet]
-        public HttpResponseMessage GetFilteredQueryForm([FromBody]Object filterParameters)
+        public HttpResponseMessage Get()
         {
             var response = new HttpResponseMessage();
+
+            //Load all data which will then be queried
             string sitePath = HttpContext.Current.Server.MapPath("~/assets/site.json");
             string siteText = System.IO.File.ReadAllText(sitePath);
             string analytePath = HttpContext.Current.Server.MapPath("~/assets/analyte.json");
             string analyteText = System.IO.File.ReadAllText(analytePath);
             string guidelinePath = HttpContext.Current.Server.MapPath("~/assets/guideline.json");
             string guidelineText = System.IO.File.ReadAllText(guidelinePath);
-            string jsonResponse = Newtonsoft.Json.JsonConvert.SerializeObject(new { sites = siteText, analytes = analyteText, guidelines = guidelineText });
+           
+
+            string jsonResponse = Newtonsoft.Json.JsonConvert.SerializeObject(new { sites = siteText, analytes = analyteText, guidelines = guidelineText});
             response.Content = new StringContent(jsonResponse);
             return response;
         }
-
 
     }
 }
--- Retheming_WIP_Nima/Hatfield.EnviroData.MVCPrototype/Scripts/scripts.js
+++ Retheming_WIP_Nima/Hatfield.EnviroData.MVCPrototype/Scripts/scripts.js
@@ -11,25 +11,54 @@ var WQViewModel = function () {
     self.analytes = ko.observable("");
     self.guidelines = ko.observable("");
     self.saved;
-    self.responseMessage = ko.observable(""); //temp
 
     self.selectedSites = ko.observableArray([]);
+    self.selectedAnalytes = ko.observableArray([]);
+    self.selectedGuidelines = ko.observableArray([]);
+    self.visibleSites;
+
+    self.packagedMessage = { testMessage: "hello" };
+
 
     this.selectedSites.subscribe(function (updatedArray) {
-        GetQueryForm(); //this will instead trigger repackaging the form and submitting it, and receiving the new values, and the form will then refresh.
+        FilterQueryForm("sites");
     });
-
-    function GetQueryForm() {
+    this.selectedAnalytes.subscribe(function (updatedArray) {
+        FilterQueryForm("analytes");
+    });
+    this.selectedGuidelines.subscribe(function (updatedArray) {
+        FilterQueryForm("guidelines");
+    });
+    function GetInitialQueryForm() {
         $.ajax({
             type: "GET",
-            url: "http://localhost:51683/WQ/QueryData",
+            url: "http://localhost:51683/WQ/LoadForm",
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function (data) {
-                self.sites(data.sites);
-                self.analytes(data.analytes);
-                self.guidelines(data.guidelines);
+                self.sites(JSON.parse(data.sites));
+                self.analytes(JSON.parse(data.analytes));
+                self.guidelines(JSON.parse(data.guidelines));
 
+            },
+            error: function (error) {
+                alert(error.status + "<--and--> " + error.statusText);
+            }
+        });
+        //Ends Here
+    }
+
+    function FilterQueryForm(modifiedFormId) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:51683/WQ/QueryData",
+            data: { modifiedFormId: modifiedFormId, selectedSites: selectedSites, selectedAnalytes: selectedAnalytes, selectedGuidelines: selectedGuidelines },
+            success: function (data) {
+                var temp = JSON.parse(data);
+                self.sites(temp.sites);
+                self.analytes(temp.analytes);
+                self.guidelines(temp.guidelines);
+                alert(temp.message);
             },
             error: function (error) {
                 alert(error.status + "<--and--> " + error.statusText);
@@ -89,7 +118,7 @@ var WQViewModel = function () {
     }
 
     //TODO: switch block so these only run on relevant pages
-    GetQueryForm();
+    GetInitialQueryForm();
     GetHomeInfo();
     GetProvisionalDatasets();
     GetMonitoringSites();
